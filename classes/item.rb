f7c0f18603463124e2f982e2f53507e66b5d2a3e
shayan1234554321 <kshayan091@gmail.com>
2023-05-30 02:28:57 +0500

    Implement methods

--- classes/item.rb
+++ classes/item.rb
@@ -1,13 +1,11 @@
+require 'date'
+
 class Item
-  attr_reader :id, :publish_date
-  attr_accessor :genre, :author, :source, :lable
+  attr_reader :id, :publish_date, :genre, :author, :source, :lable
+
   def initialize(publish_date)
     @id = Random.rand(1..10000)
-    @genre = 
-    @author =
-    @source =
-    @lable =
-    @publish_date =
+    @publish_date = Date.parse(publish_date)
     @archived = false
   end
 
@@ -15,12 +13,25 @@ class Item
     @genre = genre
   end
 
+  def set_author(author)
+    @author = author
+  end
+
+  def set_source(source)
+    @source = source
+  end
+
+  def set_lable(lable)
+    @lable = lable
+  end
+
   def move_to_archive
-    @archived = true
+    @archived = true if can_be_archived?
   end
 
   private
   def can_be_archived?
-    
+    current_date = Date.today
+    @publish_date.year + 10 < current_date.year ? true : false
   end
 end
