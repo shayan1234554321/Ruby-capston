4f99dd719f2c4ee899217a8efcb5bdc887ded712
shayan1234554321 <kshayan091@gmail.com>
2023-05-31 17:08:41 +0500

    added game class and schema

---
+++ classes/game.rb
@@ -0,0 +1,16 @@
+require_relative 'item'
+
+class Game < Item
+
+    attr_accessor :multiplayer, :last_played_at
+
+    def initialize(publish_date , multiplayer, last_played_at , id)
+        super(publish_date ,id)
+        @multiplayer = multiplayer
+        @last_played_at = last_played_at
+    end
+
+    def can_be_archived?
+        super && (@last_played_at.year + 2) < Date.today.year
+    end 
+end
