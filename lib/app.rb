d136219a011ad8a7160f12b6d89c84d5458cfbd0
MARK RUBANZA <markrubanza10@gmail.com>
2023-05-31 18:33:22 +0300

    preserve data

--- lib/app.rb
+++ lib/app.rb
@@ -1,5 +1,59 @@
+require_relative '../classes/genre'
+require_relative '../classes/music_album'
+
+require '../file_handler'
+
 class App
-  def list_items
-    puts 'list'
+  attr_accessor :genres, :music_albums
+
+  include FileHandler
+
+  def initialize
+    Genre.load
+    MusicAlbum.load
+
+    @genres = Genre.all
+    @music_albums = MusicAlbum.all
   end
-end
+
+  def list_genres
+    if @genres.empty?
+      puts 'No genres registered.'
+    else
+      puts '### Genres ###'
+      @genres.each_with_index { |genre, index| puts "#{index + 1} - #{genre.name}" }
+    end
+  end
+
+  def list_music_albums
+    if @music_albums.empty?
+      puts 'No albums found.'
+    else
+      puts '### Music Albums ###'
+      @music_albums.each do |album|
+        puts "
+      Genre: #{album.genre}
+      Author: #{album.author}
+      Source: #{album.source}
+      Label: #{album.label}
+      Publish Date: #{album.publish_date}
+      On Spotify: #{album.on_spotify}
+      "
+      end
+    end
+
+    def add_genre
+      Genre.create
+    end
+
+    def add_music_album
+      MusicAlbum.create
+    end
+
+    def exit
+      FileHandler.save(@genres, 'genres.json') if @genres.any?
+      FileHandler.save(@music_albums, 'music_albums.json') if @music_albums.any?
+      puts 'Thanks for using this app!'
+      exit!
+    end
+  end
